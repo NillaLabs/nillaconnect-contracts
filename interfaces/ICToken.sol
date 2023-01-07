d5f725b1af8f8c1ae1baf179f8955275a2470a93
Kittikorn Leelawanich <kittikornleelawanich@Kittikorns-MacBook-Pro.local>
2023-01-08 05:14:39 +0700

    Add interfaces

---
+++ interfaces/ICToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
+
+interface ICToken is IERC20 {
+    function decimals() external view returns (uint8);
+
+    function underlying() external view returns (address);
+
+    function mint(uint256) external returns (uint256);
+
+    function redeem(uint256) external returns (uint256);
+}
