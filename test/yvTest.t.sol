62dcb0e40201d391f28cb0f50726028029c7a93f
Sitthiput <sitthiput.m@gmail.com>
2023-01-16 13:18:42 +0700

    update: add proxy in setUp()

--- test/yvTest.t.sol
+++ test/yvTest.t.sol
@@ -3,10 +3,12 @@ pragma solidity 0.8.17;
 import "forge-std/Test.sol";
 import "../contracts/ProxyAdminImpl.sol";
 import "../contracts/TransparentUpgradeableProxyImpl.sol";
+import "../contracts/vaults/YearnNillaVault.sol";
 
 contract YVTest is Test {
-    ProxyAdminImpl internal proxyAdmin;
-    TransparentUpgradeableProxyImpl internal transparentUpgradeableProxyImpl;
+    ProxyAdminImpl internal admin;
+    TransparentUpgradeableProxyImpl internal proxy;
+    address impl;
 
     uint256 mainnetFork;
 
@@ -14,6 +16,14 @@ contract YVTest is Test {
         mainnetFork = vm.createFork("https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
         vm.selectFork(mainnetFork);
         
+        admin = address(new ProxyAdminImpl());
+        impl  = address(new YearnNillaVault());
+
+        proxy = new TransparentUpgradeableProxyImpl(
+            impl,
+            admin,
+            // NOTE: TO DO- abi.encodeWithSelector(Contract.initialize.selector, param1, param2, ...);
+        );
     }
 
     function testDeposit() public {
