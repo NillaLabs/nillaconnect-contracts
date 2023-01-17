1f7846b873cf7328719dccec05e0379d12f41024
M.Sitthibudhi <sitthiput.m@gmail.com>
2023-01-17 17:27:18 +0700

    update: add SafeErc20

--- test/yvTest.t.sol
+++ test/yvTest.t.sol
@@ -5,8 +5,11 @@ import "../contracts/ProxyAdminImpl.sol";
 import "../contracts/TransparentUpgradeableProxyImpl.sol";
 import "../contracts/vaults/YearnNillaVault.sol";
 import "../interfaces/IYVToken.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract YVTest is Test {
+    using SafeERC20 for IERC20;
     
     TransparentUpgradeableProxyImpl internal proxy;
     address internal impl;
@@ -18,10 +21,17 @@ contract YVTest is Test {
 
     // zero-address
     address internal ZERO_ADDRESS = address(0);
+    
+    // vault
+    YearnNillaVault internal vault;
 
     uint256 mainnetFork;
 
-    IYVToken internal yvToken = IYVToken(0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9);
+    IERC20 baseToken; 
+    IYVToken internal yvToken = IYVToken(address(0xa258C4606Ca8206D8aA700cE2143D7db854D168c));
+
+    event Deposit(address indexed depositor, address indexed receiver, uint256 amount);
+    event Withdraw(address indexed withdrawer, address indexed receiver, uint256 amount, uint256 maxLoss);
 
     function setUp() public {
         mainnetFork = vm.createFork("https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
@@ -36,63 +46,66 @@ contract YVTest is Test {
             admin,
             abi.encodeWithSelector(YearnNillaVault.initialize.selector, yvToken, "USDC Vault", "USDC", 1, 1, executor, address(0))
         );
+
+        startHoax(address(executor));
+
+        vault = YearnNillaVault(address(proxy));
+        baseToken = IERC20(address(vault.baseToken()));
+        deal(address(baseToken), address(executor), 10_000_000);
+        deal(address(baseToken), address(vault), 10_000_000);
     }
 
     function testDepositNormal() public {
-        uint256 amount = 10_000;
-        YearnNillaVault(proxy).deposit(amount, receiver);
+        console.log("Vault address:", address(vault));
+        console.log("Vault balance:", baseToken.balanceOf(address(vault)));
+        console.log("Yearn total supply:", yvToken.totalSupply());
+        console.log("Yearn deposit limit:", yvToken.depositLimit());
+        console.log("Yearn vault api:", yvToken.apiVersion());
+        console.log("Yearn vault name:", yvToken.name());
+
+        uint256 amount = 1_000;
+        baseToken.safeApprove(address(vault), 1_000_000);
+        vault.deposit(amount, receiver);
     }
 
-    function testDepositZeroAmount() public {
-        uint256 amount = 0;
-        // deposit(amount, receiver)
-        // expect revert?
-    }
+    // function testDepositZeroAmount() public {
+    //     uint256 amount = 0;
+    //     YearnNillaVault(address(proxy)).deposit(amount, receiver);
+    //     // vm.expectRevert()
+    //     // expect revert?
+    // }
 
-    function testDepositZeroAddress() public {
-        uint256 amount = 10_000;
-        // deposit(amount, ZERO_ADDRESS)
-        // expect revert?
-    }
+    // function redeemNormal() public {
+    //     uint256 shares = 100; //how to check share owned?
+    //     uint256 maxLoss = 1;
+    //     YearnNillaVault(address(proxy)).redeem(shares, receiver, maxLoss);
+    // }
 
-    function redeemNormal() public {
-        uint256 shares = 100; //how to check share owned?
-        uint256 maxLoss = 1;
-        // redeem(shares, receiver, maxLoss)
-    }
+    // function redeemExceedingShares() public {
+    //     uint256 shares = 1_000_000;
+    //     uint256 maxLoss = 1;
+    //     YearnNillaVault(address(proxy)).redeem(shares, receiver, maxLoss);
+    //     // revert
+    // }
 
-    function redeemExceedingShares() public {
-        uint256 shares = 1_000_000;
-        uint256 maxLoss = 1;
-        // redeem(shares, receiver, maxLoss)
-        // revert
-    }
+    // function redeemZeroShare() public {
+    //     uint256 shares = 0;
+    //     uint256 maxLoss = 1;
+    //     YearnNillaVault(address(proxy)).redeem(shares, receiver, maxLoss);
+    //     // revert
+    // }
 
-    function redeemZeroShare() public {
-        uint256 shares = 0;
-        uint256 maxLoss = 1;
-        // redeem(shares, receiver, maxLoss)
-        // revert
-    }
+    // function redeemExceedingMaxLoss() public {
+    //     uint256 shares = 100;
+    //     uint256 maxLoss = 10_100;
+    //     YearnNillaVault(address(proxy)).redeem(shares, receiver, maxLoss);
+    //     // revert
+    // }
 
-    function redeemZeroAddress() public {
-        uint256 shares = 100;
-        uint256 maxLoss = 1;
-        // redeem(shares, ZERO_ADDRESS, maxLoss)
-        // revert
-    }
-
-    function redeemExceedingMaxLoss() public {
-        uint256 shares = 100;
-        uint256 maxLoss = 10_100;
-        // redeem(shares, receiver, maxLoss)
-        // revert
-    }
-
-    function redeemZeroMaxLoss() public {
-        uint256 shares = 100;
-        uint256 maxLoss = 0;
-        // redeem(shares, receiver, maxLoss)
-        // revert
-    }
+    // function redeemZeroMaxLoss() public {
+    //     uint256 shares = 100;
+    //     uint256 maxLoss = 0;
+    //     YearnNillaVault(address(proxy)).redeem(shares, receiver, maxLoss);
+    //     // revert
+    // }
 }
