394a01dd0948bea1a3f9f654e74d1e058abf83bb
Sitthiput <sitthiput.m@gmail.com>
2023-02-09 00:16:49 +0700

    add: init aave-v3 contract

---
+++ contracts/lending_pools/AaveV3NillaLendingPool.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/utils/SafeERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/utils/math/Math.sol";
+
+import "../BaseNillaEarn.sol";
+
+import "../../interfaces/IAToken.sol";
+import "../../interfaces/IAaveV2LendingPool.sol";
+
+contract AaveV2NillaLendingPool is BaseNillaEarn {
+    using SafeERC20 for IERC20;
+    using Math for uint256;
+
+    IAToken public aToken;
+    IERC20 public baseToken;
+    uint8 private _decimals;
+    IAaveV2LendingPool public lendingPool;
+
+    uint256 private constant RAY = 1e27;
+
+    event Deposit(address indexed depositor, address indexed receiver, uint256 amount);
+    event Withdraw(address indexed withdrawer, address indexed receiver, uint256 amount);
+
+    function initialize(
+        IAaveV2LendingPool _lendingPool,
+        IAToken _aToken,
+        string memory _name,
+        string memory _symbol,
+        uint16 _depositFeeBPS,
+        uint16 _withdrawFeeBPS,
+        address _executor,
+        address _bridge
+    ) external {
+        __initialize__(_name, _symbol, _depositFeeBPS, _withdrawFeeBPS, _executor, _bridge);
+        lendingPool = _lendingPool;
+        aToken = _aToken;
+        IERC20 _baseToken = IERC20(_aToken.underlyingAssetAddress());
+        baseToken = _baseToken;
+        _baseToken.safeApprove(address(_lendingPool), type(uint256).max);
+        _decimals = _aToken.decimals();
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function deposit(uint256 _amount, address _receiver) external nonReentrant {
+        // gas saving
+        IERC20 _baseToken = baseToken;
+        IAToken _aToken = aToken;
+        // transfer fund.
+        uint256 baseTokenBefore = _baseToken.balanceOf(address(this));
+        _baseToken.safeTransferFrom(msg.sender, address(this), _amount);
+        uint256 receivedBaseToken = _baseToken.balanceOf(address(this)) - baseTokenBefore;
+        // deposit to Aave v2.
+        // using share not rebase token.
+        uint256 aTokenShareBefore = _aToken.scaledBalanceOf(address(this));
+        lendingPool.deposit(address(_baseToken), _amount, address(this), 0);
+        uint256 received = _aToken.scaledBalanceOf(address(this)) - aTokenShareBefore;
+        // collect protocol's fee.
+        uint256 depositFee = (received * depositFeeBPS) / BPS;
+        reserves[address(_aToken)] += depositFee;
+        _mint(_receiver, received - depositFee);
+        emit Deposit(msg.sender, _receiver, _amount);
+    }
+
+    function redeem(uint256 _shares, address _receiver) external nonReentrant {
+        // gas saving
+        IAaveV2LendingPool _lendingPool = lendingPool;
+        address _baseToken = address(baseToken);
+        IAToken _aToken = aToken;
+        // set msgSender for cross chain tx.
+        address msgSender = _msgSender(_receiver);
+        // burn user's shares
+        _burn(msgSender, _shares);
+        // collect protocol's fee.
+        uint256 withdrawFee = (_shares * withdrawFeeBPS) / BPS;
+        uint256 shareAfterFee = _shares - withdrawFee;
+        // withdraw user's fund.
+        uint256 aTokenShareBefore = _aToken.scaledBalanceOf(address(this));
+        uint256 receivedBaseToken = _lendingPool.withdraw(
+            _baseToken,
+            shareAfterFee.mulDiv(
+                _lendingPool.getReserveNormalizedIncome(_baseToken),
+                RAY,
+                Math.Rounding.Down
+            ), // aToken amount rounding down
+            msg.sender == executor ? address(this) : _receiver
+        );
+        uint256 burnedATokenShare = _aToken.scaledBalanceOf(address(this)) - aTokenShareBefore;
+        // dust after burn rounding.
+        uint256 dust = shareAfterFee - burnedATokenShare;
+        reserves[address(aToken)] += (withdrawFee + dust);
+        // bridge token back if cross chain tx.
+        if (msg.sender == executor) {
+            _bridgeTokenBack(_receiver, receivedBaseToken);
+            emit Withdraw(msg.sender, bridge, receivedBaseToken);
+        }
+        // else transfer fund to user.
+        else emit Withdraw(msg.sender, _receiver, receivedBaseToken);
+    }
+
+    function withdrawReserve(address _token, uint256 _amount) external override {
+        require(msg.sender == worker, "only worker");
+        IAToken _aToken = aToken; // gas saving
+        if (_token != address(aToken)) {
+            reserves[_token] -= _amount;
+            IERC20(_token).safeTransfer(msg.sender, _amount);
+            emit WithdrawReserve(msg.sender, _token, _amount);
+        } else {
+            // using shares for aToken
+            uint256 aTokenShareBefore = _aToken.scaledBalanceOf(address(this));
+            IERC20(_token).safeTransfer(msg.sender, _amount);
+            uint256 transferedATokenShare = (_aToken.scaledBalanceOf(address(this)) -
+                aTokenShareBefore);
+            reserves[_token] -= transferedATokenShare;
+            emit WithdrawReserve(msg.sender, _token, transferedATokenShare);
+        }
+    }
+}
