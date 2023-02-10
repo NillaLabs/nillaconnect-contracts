a563e831f6002df80337a2ae0feddfe87fb19f04
M.Sitthibudhi <sitthiput.m@gmail.com>
2023-02-10 14:35:37 +0700

    update: fix code

--- contracts/lending_pools/AaveV3NillaLendingPool.sol
+++ contracts/lending_pools/AaveV3NillaLendingPool.sol
@@ -8,17 +8,17 @@ import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/utils/math/Math.sol"
 
 import "../BaseNillaEarn.sol";
 
-import "../../interfaces/IAToken.sol";
-import "../../interfaces/IAaveV2LendingPool.sol";
+import "../../interfaces/IATokenV3.sol";
+import "../../interfaces/IAaveV3LendingPool.sol";
 
 contract AaveV2NillaLendingPool is BaseNillaEarn {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
-    IAToken public aToken;
+    IATokenV3 public aToken;
     IERC20 public baseToken;
     uint8 private _decimals;
-    IAaveV2LendingPool public lendingPool;
+    IAaveV3LendingPool public lendingPool;
 
     uint256 private constant RAY = 1e27;
 
@@ -26,8 +26,8 @@ contract AaveV2NillaLendingPool is BaseNillaEarn {
     event Withdraw(address indexed withdrawer, address indexed receiver, uint256 amount);
 
     function initialize(
-        IAaveV2LendingPool _lendingPool,
-        IAToken _aToken,
+        address _lendingPool,
+        address _aToken,
         string memory _name,
         string memory _symbol,
         uint16 _depositFeeBPS,
@@ -36,12 +36,12 @@ contract AaveV2NillaLendingPool is BaseNillaEarn {
         address _bridge
     ) external {
         __initialize__(_name, _symbol, _depositFeeBPS, _withdrawFeeBPS, _executor, _bridge);
-        lendingPool = _lendingPool;
-        aToken = _aToken;
-        IERC20 _baseToken = IERC20(_aToken.underlyingAssetAddress());
+        lendingPool = IAaveV3LendingPool(_lendingPool);
+        aToken = IATokenV3(_aToken);
+        IERC20 _baseToken = IERC20(IATokenV3(_aToken).UNDERLYING_ASSET_ADDRESS());
         baseToken = _baseToken;
-        _baseToken.safeApprove(address(_lendingPool), type(uint256).max);
-        _decimals = _aToken.decimals();
+        _baseToken.safeApprove(_lendingPool, type(uint256).max);
+        _decimals = IATokenV3(_aToken).decimals();
     }
 
     function decimals() public view virtual override returns (uint8) {
@@ -51,35 +51,37 @@ contract AaveV2NillaLendingPool is BaseNillaEarn {
     function deposit(uint256 _amount, address _receiver) external nonReentrant {
         // gas saving
         IERC20 _baseToken = baseToken;
-        IAToken _aToken = aToken;
+        IATokenV3 _aToken = aToken;
         // transfer fund.
         uint256 baseTokenBefore = _baseToken.balanceOf(address(this));
         _baseToken.safeTransferFrom(msg.sender, address(this), _amount);
         uint256 receivedBaseToken = _baseToken.balanceOf(address(this)) - baseTokenBefore;
-        // deposit to Aave v2.
-        // using share not rebase token.
+        // supply to Aave V3, using share instead.
         uint256 aTokenShareBefore = _aToken.scaledBalanceOf(address(this));
-        lendingPool.deposit(address(_baseToken), _amount, address(this), 0);
-        uint256 received = _aToken.scaledBalanceOf(address(this)) - aTokenShareBefore;
+        lendingPool.supply(address(_baseToken), _amount, address(this), 0);
+        uint256 receivedAToken = _aToken.scaledBalanceOf(address(this)) - aTokenShareBefore;
         // collect protocol's fee.
-        uint256 depositFee = (received * depositFeeBPS) / BPS;
+        uint256 depositFee = receivedAToken.mulDiv(depositFeeBPS, BPS);
         reserves[address(_aToken)] += depositFee;
-        _mint(_receiver, received - depositFee);
+        _mint(_receiver, receivedAToken - depositFee);
         emit Deposit(msg.sender, _receiver, _amount);
     }
 
     function redeem(uint256 _shares, address _receiver) external nonReentrant {
         // gas saving
-        IAaveV2LendingPool _lendingPool = lendingPool;
+        IAaveV3LendingPool _lendingPool = lendingPool;
         address _baseToken = address(baseToken);
-        IAToken _aToken = aToken;
+        IATokenV3 _aToken = aToken;
+
         // set msgSender for cross chain tx.
         address msgSender = _msgSender(_receiver);
         // burn user's shares
         _burn(msgSender, _shares);
+
         // collect protocol's fee.
-        uint256 withdrawFee = (_shares * withdrawFeeBPS) / BPS;
+        uint256 withdrawFee = _shares.mulDiv(withdrawFeeBPS, BPS);
         uint256 shareAfterFee = _shares - withdrawFee;
+        
         // withdraw user's fund.
         uint256 aTokenShareBefore = _aToken.scaledBalanceOf(address(this));
         uint256 receivedBaseToken = _lendingPool.withdraw(
@@ -106,8 +108,8 @@ contract AaveV2NillaLendingPool is BaseNillaEarn {
 
     function withdrawReserve(address _token, uint256 _amount) external override {
         require(msg.sender == worker, "only worker");
-        IAToken _aToken = aToken; // gas saving
-        if (_token != address(aToken)) {
+        IATokenV3 _aToken = aToken; // gas saving
+        if (_token != address(_aToken)) {
             reserves[_token] -= _amount;
             IERC20(_token).safeTransfer(msg.sender, _amount);
             emit WithdrawReserve(msg.sender, _token, _amount);
@@ -115,8 +117,7 @@ contract AaveV2NillaLendingPool is BaseNillaEarn {
             // using shares for aToken
             uint256 aTokenShareBefore = _aToken.scaledBalanceOf(address(this));
             IERC20(_token).safeTransfer(msg.sender, _amount);
-            uint256 transferedATokenShare = (_aToken.scaledBalanceOf(address(this)) -
-                aTokenShareBefore);
+            uint256 transferedATokenShare = _aToken.scaledBalanceOf(address(this)) - aTokenShareBefore;
             reserves[_token] -= transferedATokenShare;
             emit WithdrawReserve(msg.sender, _token, transferedATokenShare);
         }
