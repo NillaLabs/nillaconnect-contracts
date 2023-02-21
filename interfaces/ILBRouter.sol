f25dcfa308dca4d9bff7de4be7dcf8d01c884e53
Sitthiput <sitthiput.m@gmail.com>
2023-02-21 20:15:18 +0700

    add: IRouter for swapping (TraderJoe)

---
+++ interfaces/ILBRouter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
+
+interface ILBRouter {    
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        uint256[] memory pairBinSteps,
+        IERC20[] memory tokenPath,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountOut);
+}
