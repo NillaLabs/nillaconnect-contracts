ab79ac87cc46195e2daee50e7a9d26779b515cab
mewpawee <mewpawee@Pawees-Mac-mini.local>
2023-03-01 14:47:02 +0700

    feat: support deposit token

--- scripts/utils/swap_eth_token.py
+++ scripts/utils/swap_eth_token.py
@@ -3,7 +3,6 @@ from brownie import interface, chain, network, web3, Wei
 from eth_utils import is_checksum_address
 
 UNISWAP_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-OUTPUT_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" #USDC
 
 network.priority_fee("2 gwei")
 
@@ -27,23 +26,31 @@ def get_address(msg: str, default: str = None) -> str:
         val = click.prompt(msg)
 
 def main():
+    uniswap = interface.IUniswapRouterV2(UNISWAP_ADDRESS)
+    weth_address = uniswap.WETH()
+    weth = interface.IWNative(weth_address) 
+
     sender = get_address("Sender", default="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
     receiver = get_address("Receiver", default=sender)
+    token_out = get_address("Token Address (default=WETH)", default=f"{weth_address}")
     amount_in = click.prompt("Amount(ethers)", default='1')
     amount_in_wei = Wei(f'{amount_in} ethers')
     click.secho(f"Sender: {sender}", fg='green')
     click.secho(f"Receiver: {receiver}", fg='green')
     click.secho(f"Amount: {amount_in} ethers\n", fg='green')
 
-    uniswap = interface.IUniswapRouterV2(UNISWAP_ADDRESS)
-    weth = uniswap.WETH()
-    path = [weth, OUTPUT_ADDRESS]
-    amounts  = uniswap.getAmountsOut(amount_in_wei, path)
-    click.secho(f"swapExactETHForTokens...", fg='yellow')
-    swap_amounts = uniswap.swapExactETHForTokens(amounts[1],
-                                                 path,
-                                                 receiver,
-                                                 chain.time() + 1000000000000,
-                                                 {'from': sender, 'value': amounts[0]})
-    click.secho("Done!", fg='green')
-    click.secho(swap_amounts)
+    if(token_out == weth):
+        deposit = weth.deposit({'from': sender, 'value': amount_in_wei})
+        click.secho("Done Deposit!", fg='green')
+        click.secho(deposit)
+    else:        
+        path = [weth, token_out]
+        amounts  = uniswap.getAmountsOut(amount_in_wei, path)
+        click.secho(f"swapExactETHForTokens...", fg='yellow')
+        swap_amounts = uniswap.swapExactETHForTokens(amounts[1],
+                                                     path,
+                                                     receiver,
+                                                     chain.time() + 1000000000000,
+                                                     {'from': sender, 'value': amounts[0]})
+        click.secho("Done Swap!", fg='green')
+        click.secho(swap_amounts)
