c9e6ab1c0a5511814ee2e42946fd0ca22fa145f8
Sitthiput <sitthiput.m@gmail.com>
2023-03-01 17:48:38 +0700

    update: fix code

--- contracts/liquidity_staking/LidoNilla.sol
+++ contracts/liquidity_staking/LidoNilla.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/utils/SafeERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/utils/math/Math.sol";
 
 import "../BaseNillaEarn.sol";
 
@@ -13,10 +14,11 @@ import "../../interfaces/IUniswapRouterV2.sol";
 
 contract IronBankNillaLendingPool is BaseNillaEarn {
     using SafeERC20 for IERC20;
+    using Math for uint256;
 
     ILido public lido;
     IUniswapRouterV2 public swapRouter;
-    IWNative public immutable WETH;
+    IWNative public WETH;
     IERC20 public baseToken;
     uint8 private constant _decimals = 18; // stETH's decimals is 18
 
@@ -36,8 +38,8 @@ contract IronBankNillaLendingPool is BaseNillaEarn {
         address _bridge
     ) external {
         __initialize__(_name, _symbol, _depositFeeBPS, _withdrawFeeBPS, _executor, _bridge);
-        lido = _lido;
-        swapRouter = _swapRouter;
+        lido = ILido(_lido);
+        swapRouter = IUniswapRouterV2(_swapRouter);
         WETH = IWNative(_weth);
         baseToken = IERC20(_stETH);
         IERC20(_weth).safeApprove(_swapRouter, type(uint256).max);
@@ -51,31 +53,33 @@ contract IronBankNillaLendingPool is BaseNillaEarn {
     function deposit(uint256 _amount, address _receiver) external payable nonReentrant returns (uint256) {
         // gas saving
         ILido _lido = lido;
-        // deposit to Lido Finance.
-        uint256 sharesBefore = _lido.balanceOf(address(this));
-        _lido.submit{value: msg.value}(address(this));
-        uint256 receivedShares = _lido.balanceOf(address(this)) - sharesBefore;
+        IERC20 _baseToken = baseToken;
+        // submit to Lido Finance.
+        uint256 receivedBase = _lido.submit{value: msg.value}(address(this));
         // collect protocol's fee.
-        uint256 depositFee = (receivedShares * depositFeeBPS) / BPS;
+        uint256 depositFee = (receivedBase * depositFeeBPS) / BPS;
         reserves[address(_lido)] += depositFee;
-        _mint(_receiver, receivedShares - depositFee);
+        _mint(_receiver, receivedBase - depositFee);
         emit Deposit(msg.sender, _receiver, _amount);
-        return (receivedShares - depositFee);
+        return (receivedBase - depositFee);
     }
 
     function redeem(uint256 _shares, address _receiver, uint256 _amountOutMin, address[] calldata _path, uint256 _deadline) external nonReentrant returns (uint256) {
         // gas saving
         IWNative _WETH = WETH;
+        ILido _lido = lido;
         // set msgSender for cross chain tx.
         address msgSender = _msgSender(_receiver);
         // burn user's shares
         _burn(msgSender, _shares);
         // collect protocol's fee.
         uint256 withdrawFee = (_shares * withdrawFeeBPS) / BPS;
-        reserves[address(_lido)] += withdrawFee;
+        reserves[address(lido)] += withdrawFee;
+        // convert shares to amount
+        uint256 amount = _shares.mulDiv(_lido.totalSupply(), _lido.getTotalShares());
         // swap user's fund.
         uint256 WETHBefore = IERC20(_WETH).balanceOf(address(this));
-        swapRouter.swapExactTokensForTokens(amountIn, _amountOutMin, _path, address(this), _deadline);
+        swapRouter.swapExactTokensForTokens(amount, _amountOutMin, _path, address(this), _deadline);
         uint256 receivedWETH = IERC20(_WETH).balanceOf(address(this)) - WETHBefore;
         // bridge token back if cross chain tx.
         if (msg.sender == executor) {
@@ -84,7 +88,7 @@ contract IronBankNillaLendingPool is BaseNillaEarn {
         }
         // else transfer fund to user.
         else {
-            _baseToken.safeTransfer(_receiver, receivedWETH);
+            IERC20(_WETH).safeTransfer(_receiver, receivedWETH);
             emit Withdraw(msg.sender, _receiver, receivedWETH);
         }
         return receivedWETH;
