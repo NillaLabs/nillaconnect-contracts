0965b567716e7fac26e10d50a6051b44fd035179
Sitthiput <sitthiput.m@gmail.com>
2023-03-02 14:00:04 +0700

    update: add function in interface

--- interfaces/INillaLendingPool.sol
+++ interfaces/INillaLendingPool.sol
@@ -6,13 +6,18 @@ import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.s
 
 interface INillaLendingPool is IERC20 {
     function decimals() external view returns (uint8);
-
-    function baseToken() external view returns (address);
+    function baseToken() external view returns (IERC20);
 
     function deposit(
         uint256 amount,
         address receiver
     ) external returns (uint256);
+
+    // deposit in Lido or used when depositing ETH directly
+    // No need for the `amount` to be specified
+    function deposit(
+        address receiver
+    ) external payable returns (uint256);
 
     function redeem(
         uint256 shares,
