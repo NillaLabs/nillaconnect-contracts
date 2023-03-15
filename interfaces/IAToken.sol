ec0416603d30edaec38ba5e74a1200e52c5ccd89
Sitthiput <sitthiput.m@gmail.com>
2023-03-15 23:51:30 +0700

    update: - remove IAToken - change IATokenV3 to IAToken, using the same for both version

--- interfaces/IAToken.sol
+++ interfaces/IAToken.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
 import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
 
-interface IAToken is IERC20 {
+interface IATokenV3 is IERC20 {
     function decimals() external view returns (uint8);
 
     function scaledBalanceOf(address user) external view returns (uint256);
 
-    function underlyingAssetAddress() external view returns (address);
+    function UNDERLYING_ASSET_ADDRESS() external view returns (address);
+
+    function POOL() external view returns (address);
+
+    function totalSupply() external view returns (uint256);
 }
