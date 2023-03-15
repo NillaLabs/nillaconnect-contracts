0c5f58f3f9828fbc0b50aa83f69e8080f776b6e8
Sitthiput <sitthiput.m@gmail.com>
2023-03-15 13:50:44 +0700

    script: add AaveV2 deploy script

---
+++ scripts/deploy_aave_v2.py
@@ -0,0 +1,52 @@
+import eth_utils
+import json
+
+from brownie import network, interface, Contract
+from brownie import ProxyAdminImpl, AaveV2NillaLendingPool, TransparentUpgradeableProxyImpl
+
+network.priority_fee("2 gwei")
+f_chain = open('./scripts/utils/chainId.json',)
+f_address = open('./scripts/utils/address.json',)
+data_chain_id = json.load(f_chain)
+data_address = json.load(f_address)
+
+deployer = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" # NOTE: Change address later
+
+def set_network(network):
+    return data_chain_id[network.upper()] 
+
+def set_vault(chain_id, token):
+    a_token = data_address[chain_id]['AAVEV2_ATOKEN'][token.upper()]
+    lending_pool = data_address[chain_id]['AAVEV2_LENDINGPOOL']
+    return a_token, lending_pool
+
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+def main():
+    chain_id = set_network('mainnet')
+    a_token, lending_pool = set_vault(chain_id, 'weth')
+    print(f"Network: '{network.show_active()}'")
+    print(f"Using account: [{deployer}]")
+
+    admin = ProxyAdminImpl.deploy({'from': deployer})
+    impl = AaveV2NillaLendingPool.deploy({'from': deployer})
+    aave_v2_initilize_encoded = encode_function_data(
+        impl.initialize,
+        lending_pool,
+        a_token,
+        "WETH AAVE V2 LP",
+        "naWETH",
+        3,
+        3
+    )
+    proxy_impl = TransparentUpgradeableProxyImpl.deploy(
+        impl,
+        admin,
+        aave_v2_initilize_encoded,
+        {'from': deployer}
+    )
+    proxy_vault = Contract.from_abi("AaveV2NillaLendingPool", proxy_impl.address, impl.abi)
+    print(proxy_vault)
