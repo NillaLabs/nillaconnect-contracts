ba483add6b2a5bf09bec8d1688107acbf76d208d
Sitthiput <sitthiput.m@gmail.com>
2023-03-15 15:40:13 +0700

    script: add compound deploy script

---
+++ scripts/deploy_compound.py
@@ -0,0 +1,45 @@
+import eth_utils
+import json
+
+from brownie import network, Contract
+from brownie import ProxyAdminImpl, CompoundNillaLendingPool, TransparentUpgradeableProxyImpl
+
+network.priority_fee("2 gwei")
+f_chain = open('./scripts/utils/chainId.json',)
+f_address = open('./scripts/utils/address.json',)
+data_chain_id = json.load(f_chain)
+data_address = json.load(f_address)
+
+deployer = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" # NOTE: Change address later
+
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+def set_c_token(token):
+    return data_address['1']['COMPOUND_CTOKEN'][token.upper()] 
+
+def main():
+    c_token = set_c_token('usdt')
+    print(f"Network: '{network.show_active()}'")
+    print(f"Using account: [{deployer}]")
+
+    admin = ProxyAdminImpl.deploy({'from': deployer})
+    impl = CompoundNillaLendingPool.deploy({'from': deployer})
+    compound_initilize_encoded = encode_function_data(
+        impl.initialize,
+        c_token,
+        "Compound LP - USDT",
+        "ncUSDT",
+        3,
+        3
+    )
+    proxy_impl = TransparentUpgradeableProxyImpl.deploy(
+        impl,
+        admin,
+        compound_initilize_encoded,
+        {'from': deployer}
+    )
+    proxy_vault = Contract.from_abi("CompoundNillaLendingPool", proxy_impl.address, impl.abi)
+    print(proxy_vault)
