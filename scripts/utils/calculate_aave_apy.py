e53d90871605d933cb2a6440c31ed620320b88f0
Sitthiput <sitthiput.m@gmail.com>
2023-03-23 14:20:28 +0700

    script: mock-up Aave's APY cal script

--- scripts/utils/calculate_aave_apy.py
+++ scripts/utils/calculate_aave_apy.py
@@ -1,12 +1,22 @@
-import json
-
 from brownie import network, interface
 
-from scripts.utils.utils import encode_function_data
+network.priority_fee("2 gwei")
+
+RAY = 10**27
+SECONDS_PER_YEAR = 31536000
 
 def main():
-    network.priority_fee("2 gwei")
     asset = '0xdAC17F958D2ee523a2206206994597C13D831ec7' # USDT in V3
     pool = interface.IAaveLendingPool('0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2')
+    [_, _, _, currentLiquidityRate, currentVariableBorrowRate, _, _,_, _, _, _, _] = pool.getReserveData(asset)
+    depositAPR = currentLiquidityRate/RAY
+    variableBorrowAPR = currentVariableBorrowRate/RAY
+    stableBorrowAPR = currentVariableBorrowRate/RAY
 
-    print(pool.getReserveData(asset))
+    depositAPY = ((1 + (depositAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+    variableBorrowAPY = ((1 + (variableBorrowAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+    stableBorrowAPY = ((1 + (stableBorrowAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+
+    print("Deposit APY:", depositAPY)
+    print("Variable Borrow APY:", variableBorrowAPY)
+    print("Stable Borrow APY:", stableBorrowAPY)
