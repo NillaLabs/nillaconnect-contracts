f33b5ef21c648e0f52b334803c8eea975df2d17b
Sitthiput <sitthiput.m@gmail.com>
2023-04-11 02:16:53 +0700

    script: update deploy script

--- scripts/deploy_optimism/deploy_yearn_op.py
+++ scripts/deploy_optimism/deploy_yearn_op.py
@@ -6,16 +6,18 @@ from brownie import Contract, accounts
 from brownie import ProxyAdminImpl, YearnNillaVault, TransparentUpgradeableProxyImpl, NativeGatewayVault
 from scripts.utils.utils import *
 
+network.priority_fee("2 gwei")
 load_dotenv()
 
 f_address = open('./scripts/constants/address.json', )
 data_address = json.load(f_address)
 
-deployer = Account.from_mnemonic(
-    os.getenv("MNEMONIC"))  # NOTE: Change address later
-accounts.add(deployer.privateKey)
+# deployer = Account.from_mnemonic(
+#     os.getenv("MNEMONIC"))  # NOTE: Change address later
+# accounts.add(deployer.privateKey)
+deployer = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
 
-CHAIN_ID = '10'
+CHAIN_ID = '1'
 WETH = '0x4200000000000000000000000000000000000006'
 MULTISIGWALLET = '0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f'
 PARTNER_TRACKER = data_address[CHAIN_ID]['YEARN_PARTNER_TRACKER']
@@ -28,16 +30,17 @@ def get_yv_token(token):
 
 
 def main():
-    admin = ProxyAdminImpl.deploy({'from': accounts[0]})
-    impl = YearnNillaVault.deploy({'from': accounts[0]})
-    gateway = NativeGatewayVault.deploy(WETH, {'from': accounts[0]})
+    admin = ProxyAdminImpl.deploy({'from': deployer})
+    impl = YearnNillaVault.deploy({'from': deployer})
+    gateway = NativeGatewayVault.deploy(WETH, {'from': deployer})
     for token in token_address:
         yv_token = get_yv_token(token)
         yearn_initilize_encoded = encode_function_data(
             impl.initialize, yv_token, PARTNER_TRACKER, MULTISIGWALLET,
             f"{token} Yearn Vault", "NYV" + str(token), 3, 3)
         proxy_impl = TransparentUpgradeableProxyImpl.deploy(
-            impl, admin, yearn_initilize_encoded, {'from': accounts[0]})
+            impl, admin, yearn_initilize_encoded, {'from': deployer})
         proxy_vault = Contract.from_abi("YearnNillaVault", proxy_impl.address,
                                         impl.abi)
-        print(f'Proxy Vault {token}:', proxy_vault)
+        print(f'Proxy Vault {token}:', proxy_vault,
+              '\n -----------------------------------------------------')
