b88b2845b540a2ca2d5862cff96142b7697cb952
Sitthiput <sitthiput.m@gmail.com>
2023-04-18 19:02:16 +0700

    script: change AaveV3 to NoRewards contract

--- scripts/network_optimism/first_launch.py
+++ scripts/network_optimism/first_launch.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from eth_account import Account
 from brownie import Contract, accounts
-from brownie import ProxyAdminImpl, AaveV2NillaLendingPool, AaveV3NillaLendingPool, CompoundNillaLendingPool, LidoNillaLiquidityStaking, YearnNillaVault, TransparentUpgradeableProxyImpl, TransparentUpgradeableProxyImplNative, NativeGateway, NativeGatewayVault
+from brownie import ProxyAdminImpl, AaveV3NillaLendingPoolNoRewards, YearnNillaVault, TransparentUpgradeableProxyImpl, TransparentUpgradeableProxyImplNative, NativeGateway, NativeGatewayVault
 from scripts.utils.utils import *
 
 network.gas_price("0.001 gwei")
@@ -13,6 +13,8 @@ load_dotenv()
 CHAIN_ID = set_network('optimism')
 
 f_address = open('./scripts/constants/address.json', )
+admin_abi = json.load(open('./build/contracts/ProxyadminImpl.json', ))
+
 data_address = json.load(f_address)
 
 aave_v3_address = data_address[CHAIN_ID]['AAVEV3_ATOKEN']
@@ -20,73 +22,59 @@ yearn_address = data_address[CHAIN_ID]['YEARN_VAULT']
 
 WETH = data_address[CHAIN_ID]['WETH']
 AAVE_V3_POOL = data_address[CHAIN_ID]['AAVEV3_POOL']
-AAVE_V3_REWARDS_CONTROLLER = data_address[CHAIN_ID]['AAVEV3_REWARDS_CONTROLLER']
 YEARN_PARTNER_TRACKER = data_address[CHAIN_ID]['YEARN_PARTNER_TRACKER']
 
 DEPOSIT_FEE_BPS = 3
 WITHDRAW_FEE_BPS = 3
-HARVEST_FEE_BPS = 3
 
-# NOTE: Uncomment this when deploying on main.
-# deployer = Account.from_mnemonic(
-#     os.getenv("MNEMONIC"))  # NOTE: Change address later
-# accounts.add(deployer.privateKey)
-deployer = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-HARVEST_BOT = '0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f' # NOTE Change later
+#  NOTE: Uncomment this when deploying on main.
+deployer = Account.from_mnemonic(
+    os.getenv("MNEMONIC"))  # NOTE: Change address later
+accounts.add(deployer.privateKey)
+
+# deployer = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+HARVEST_BOT = '0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f'  # NOTE Change later
 WORKER_BOT = '0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f'  # NOTE Change later
-MULTISIG_WALLET = '0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f'
+MULTISIG_WALLET = '0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f'  # OP's
 
 
 def main():
     # Can globally deploy once for each network!
-    admin = ProxyAdminImpl.deploy({'from': deployer})
-    gateway = NativeGateway.deploy(WETH, {'from': deployer})
-    gateway_vault = NativeGatewayVault.deploy(WETH, {'from': deployer})
+    admin = Contract.from_abi("ProxyAdminImpl",
+                              "0x85774d5Fc82EDC9633624c009F0edfAD2DDebA1C",
+                              admin_abi['abi'])
+    # gateway = NativeGateway.deploy(WETH, {'from': accounts[0]})
+    # gateway_vault = NativeGatewayVault.deploy(WETH, {'from': accounts[0]})
 
     # ---------- Deploy Yearn's ----------
-    impl_yearn = YearnNillaVault.deploy({'from': deployer})
-    for token in yearn_address:
-        yearn_initilize_encoded = encode_function_data(
-            impl_yearn.initialize,
-            yearn_address[token],
-            YEARN_PARTNER_TRACKER,
-            MULTISIG_WALLET,
-            f"{token} Yearn-Nilla Vault",
-            "nyv" + str(token),
-            DEPOSIT_FEE_BPS,
-            WITHDRAW_FEE_BPS
-        )
-        proxy_impl_yearn = TransparentUpgradeableProxyImpl.deploy(
-            impl_yearn, admin, yearn_initilize_encoded, {'from': deployer})
-        yearn_vault = Contract.from_abi("YearnNillaVault", proxy_impl_yearn.address,
-                                        impl_yearn.abi)
-        print(f'Yearn:- Proxy Vault {token}:', yearn_vault,
-              '\n -----------------------------------------------------')
-    
+    # impl_yearn = YearnNillaVault.deploy({'from': accounts[0]})
+    # for token in yearn_address:
+    #     yearn_initilize_encoded = encode_function_data(
+    #         impl_yearn.initialize, yearn_address[token], YEARN_PARTNER_TRACKER,
+    #         MULTISIG_WALLET, f"{token} Yearn-Nilla Vault", "nyv" + str(token),
+    #         DEPOSIT_FEE_BPS, WITHDRAW_FEE_BPS)
+    #     proxy_impl_yearn = TransparentUpgradeableProxyImpl.deploy(
+    #         impl_yearn, admin, yearn_initilize_encoded, {'from': accounts[0]})
+    #     yearn_vault = Contract.from_abi("YearnNillaVault",
+    #                                     proxy_impl_yearn.address,
+    #                                     impl_yearn.abi)
+    #     print(f'Yearn:- Proxy Vault {token}:', yearn_vault,
+    #           '\n -----------------------------------------------------')
+
     # ---------- Deploy AAVE V3's ----------
-    impl_aave_v3 = AaveV3NillaLendingPool.deploy(
-        AAVE_V3_REWARDS_CONTROLLER,
-        WETH,
-        AAVE_V3_POOL,
-        {'from': deployer})
+    impl_aave_v3_no_rewards = AaveV3NillaLendingPoolNoRewards.deploy(
+        WETH, AAVE_V3_POOL, {'from': accounts[0]})
+
     for token in aave_v3_address:
         aave_v3_initilize_encoded = encode_function_data(
-            impl_aave_v3.initialize,
-            aave_v3_address[token],
-            '0x0',
-            HARVEST_BOT,
-            f"{token} AAVE V3-Nilla LP",
-            "na" + str(token),
-            DEPOSIT_FEE_BPS,
-            WITHDRAW_FEE_BPS,
-            HARVEST_FEE_BPS
-        )
-        proxy_impl_aave_v3 = TransparentUpgradeableProxyImplNative.deploy(
-            impl_aave_v3,
-            admin,
-            aave_v3_initilize_encoded,
-            WETH,
-            {'from': deployer}
-        )
-        aave_v3_lp = Contract.from_abi("AaveV3NillaLendingPool", proxy_impl_aave_v3.address, impl_aave_v3.abi)
-        print(f'AAVE V3:- Proxy LP {token}', aave_v3_lp, '\n -----------------------------------------------------')
+            impl_aave_v3_no_rewards.initialize, aave_v3_address[token],
+            f"{token} AAVE V3-Nilla LP", "na" + str(token), DEPOSIT_FEE_BPS,
+            WITHDRAW_FEE_BPS)
+        proxy_impl_aave_v3_no_rewards = TransparentUpgradeableProxyImplNative.deploy(
+            impl_aave_v3_no_rewards, admin, aave_v3_initilize_encoded, WETH,
+            {'from': accounts[0]})
+        aave_v3_lp = Contract.from_abi("AaveV3NillaLendingPool",
+                                       proxy_impl_aave_v3_no_rewards.address,
+                                       impl_aave_v3_no_rewards.abi)
+        print(f'AAVE V3:- Proxy LP {token}', aave_v3_lp,
+              '\n -----------------------------------------------------')
