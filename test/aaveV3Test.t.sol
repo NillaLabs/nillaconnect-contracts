01e5f1be2f0bd6a8da2bac98df2f4eb96af2dc34
Sitthiput <sitthiput.m@gmail.com>
2023-04-21 15:28:53 +0700

    test: testing protocol fee

--- test/aaveV3Test.t.sol
+++ test/aaveV3Test.t.sol
@@ -6,7 +6,7 @@ import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/utils/Sa
 
 import "../contracts/ProxyAdminImpl.sol";
 import "../contracts/TransparentUpgradeableProxyImplNative.sol";
-import "../contracts/lending_pools/AaveV3NillaLendingPool.sol";
+import "../contracts/lending_pools/AaveV3NillaLendingPoolNoRewards.sol";
 
 import "../interfaces/IAToken.sol";
 import "../interfaces/IRewardsController.sol";
@@ -16,7 +16,7 @@ import "../interfaces/IWNative.sol";
 contract AaveV3Test is Test {
     using SafeERC20 for IERC20;
     using Math for uint256;
-    
+
     TransparentUpgradeableProxyImplNative public proxy;
     address public impl;
     address public admin;
@@ -24,55 +24,52 @@ contract AaveV3Test is Test {
     address public user = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address public bot = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 
-    address public WETH = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+    address public WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     // zero-address
     address public ZERO_ADDRESS = address(0);
 
-    uint256 public avalancheFork;
+    uint256 public mainnetFork;
 
     IERC20 public baseToken;
     IAToken public aToken = IAToken(0x6ab707Aca953eDAeFBc4fD23bA73294241490620);
-    IRewardsController rewardsController = IRewardsController(0x929EC64c34a17401F460460D4B9390518E5B473e);
-    IUniswapRouterV2 swapRouter = IUniswapRouterV2(0x60aE616a2155Ee3d9A68541Ba4544862310933d4);
+    // IRewardsController rewardsController =
+    //     IRewardsController(0x929EC64c34a17401F460460D4B9390518E5B473e);
+    // IUniswapRouterV2 swapRouter = IUniswapRouterV2(0x60aE616a2155Ee3d9A68541Ba4544862310933d4);
 
-    AaveV3NillaLendingPool public aaveV3Pool;
+    AaveV3NillaLendingPoolNoRewards public aaveV3Pool;
 
     function setUp() public {
-        avalancheFork = vm.createFork("https://avalanche-mainnet.infura.io/v3/e6282a54498e433a87766276d1d4b67b"); // Avalanche
-        vm.selectFork(avalancheFork);
+        mainnetFork = vm.createFork(
+            "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+        ); // Avalanche :- temp mainnet
+        vm.selectFork(mainnetFork);
         startHoax(user);
 
         vm.label(user, "#### User ####");
         vm.label(address(aToken), "#### AToken ####");
         vm.label(WETH, "#### WETH ####");
-        vm.label(address(rewardsController), "#### Reward Controller ####");
-        vm.label(address(swapRouter), "#### Swap Router ####");
+        // vm.label(address(rewardsController), "#### Reward Controller ####");
+        // vm.label(address(swapRouter), "#### Swap Router ####");
 
         admin = address(new ProxyAdminImpl());
-        impl  = address(new AaveV3NillaLendingPool(
-                            address(rewardsController),
-                            address(WETH),
-                            address(aToken)
-                        ));
+        impl = address(new AaveV3NillaLendingPoolNoRewards(address(WETH), address(aToken)));
 
         proxy = new TransparentUpgradeableProxyImplNative(
             impl,
             admin,
             abi.encodeWithSelector(
-                AaveV3NillaLendingPool.initialize.selector,
+                AaveV3NillaLendingPoolNoRewards.initialize.selector,
                 address(aToken),
-                address(swapRouter),
-                bot,
-                "Nilla-AaveV3 USDT LP",
-                "nUSDT",
-                1,
-                1,
-                1
+                "Nilla-AaveV3 WETH LP",
+                "nWETH",
+                0,
+                0,
+                500 // performance fee 5%
             ),
             WETH
         );
-        aaveV3Pool = AaveV3NillaLendingPool(payable(address(proxy)));
+        aaveV3Pool = AaveV3NillaLendingPoolNoRewards(payable(address(proxy)));
         aaveV3Pool.setWorker(user);
         baseToken = IERC20(aaveV3Pool.baseToken());
 
@@ -89,15 +86,15 @@ contract AaveV3Test is Test {
 
         uint256 aTokenBefore = aToken.scaledBalanceOf(address(aaveV3Pool));
         uint256 reserveBefore = aaveV3Pool.reserves(address(aToken));
-
+        console.log("Prin B:", aaveV3Pool.principals(user));
         aaveV3Pool.deposit(amount, user);
-
+        console.log("Prin A:", aaveV3Pool.principals(user));
         uint256 aTokenAfter = aToken.scaledBalanceOf(address(aaveV3Pool));
         uint256 reserveAfter = aaveV3Pool.reserves(address(aToken));
-        uint256 depositFee = (aTokenAfter - aTokenBefore) * 1 / 10_000;
+        uint256 depositFee = ((aTokenAfter - aTokenBefore) * 1) / 10_000;
 
         assertEq(reserveAfter - reserveBefore, depositFee);
-        assertEq(aaveV3Pool.balanceOf(user), aTokenAfter - depositFee); 
+        assertEq(aaveV3Pool.balanceOf(user), aTokenAfter - depositFee);
 
         console.log("LP balance in aave before:", aTokenBefore);
         console.log("LP balance in aave after:", aTokenAfter);
@@ -117,10 +114,10 @@ contract AaveV3Test is Test {
 
         uint256 aTokenAfter = aToken.scaledBalanceOf(address(aaveV3Pool));
         uint256 reserveAfter = aaveV3Pool.reserves(address(aToken));
-        uint256 depositFee = (aTokenAfter - aTokenBefore) * 1 / 10_000;
+        uint256 depositFee = ((aTokenAfter - aTokenBefore) * 1) / 10_000;
 
         assertEq(reserveAfter - reserveBefore, depositFee);
-        assertEq(aaveV3Pool.balanceOf(user), aTokenAfter - depositFee); 
+        assertEq(aaveV3Pool.balanceOf(user), aTokenAfter - depositFee);
     }
 
     function testDepositTooLarge() public {
@@ -155,15 +152,16 @@ contract AaveV3Test is Test {
         aaveV3Pool.deposit(amount, user);
 
         uint256 aTokenAfterDeposit = aToken.scaledBalanceOf(address(aaveV3Pool));
-        uint256 shares = aaveV3Pool.balanceOf(user);  // Redeem total shares
+        uint256 shares = aaveV3Pool.balanceOf(user); // Redeem total shares
         uint256 withdrawFee = shares.mulDiv(1, 10_000);
         uint256 reserveBefore = aaveV3Pool.reserves(address(aToken));
-        
+
         vm.warp(block.timestamp + 1_000_000);
         aaveV3Pool.redeem(shares, user);
 
         uint256 addedReserve = aaveV3Pool.reserves(address(aToken)) - reserveBefore;
-        uint256 burnedATokenShare = aTokenAfterDeposit - aToken.scaledBalanceOf(address(aaveV3Pool));
+        uint256 burnedATokenShare = aTokenAfterDeposit -
+            aToken.scaledBalanceOf(address(aaveV3Pool));
         uint256 dust = (shares - withdrawFee) - burnedATokenShare;
 
         assertEq(addedReserve, withdrawFee + dust);
@@ -185,16 +183,17 @@ contract AaveV3Test is Test {
         deal(address(baseToken), user, amount);
         aaveV3Pool.deposit(amount, user);
 
-        uint256 shares = aaveV3Pool.balanceOf(user);  // Redeem total shares
+        uint256 shares = aaveV3Pool.balanceOf(user); // Redeem total shares
         uint256 aTokenAfterDeposit = aToken.scaledBalanceOf(address(aaveV3Pool));
         uint256 withdrawFee = shares.mulDiv(1, 10_000);
         uint256 reserveBefore = aaveV3Pool.reserves(address(aToken));
-        
+
         vm.warp(block.timestamp + 1_000_000);
         aaveV3Pool.redeem(shares, user);
 
         uint256 addedReserve = aaveV3Pool.reserves(address(aToken)) - reserveBefore;
-        uint256 burnedATokenShare = aTokenAfterDeposit - aToken.scaledBalanceOf(address(aaveV3Pool));
+        uint256 burnedATokenShare = aTokenAfterDeposit -
+            aToken.scaledBalanceOf(address(aaveV3Pool));
         uint256 dust = (shares - withdrawFee) - burnedATokenShare;
 
         assertEq(addedReserve, withdrawFee + dust);
@@ -208,7 +207,7 @@ contract AaveV3Test is Test {
 
         uint256 shares = aaveV3Pool.balanceOf(user);
         vm.expectRevert(bytes("ERC20: burn amount exceeds balance"));
-        aaveV3Pool.redeem(shares * 10**10, user);
+        aaveV3Pool.redeem(shares * 10 ** 10, user);
     }
 
     function testRedeemInvalidAmount() public {
@@ -229,37 +228,45 @@ contract AaveV3Test is Test {
         aaveV3Pool.redeem(10_000, address(0));
     }
 
-    function testReinvest() public {
-        console.log("---------- TEST NORMAL REDEEM ----------");
-        uint256 amount = 1e9;
-        deal(address(baseToken), user, amount);
+    // function testReinvest() public {
+    //     console.log("---------- TEST NORMAL REDEEM ----------");
+    //     uint256 amount = 1e9;
+    //     deal(address(baseToken), user, amount);
 
-        uint256 aTokenBefore = aToken.scaledBalanceOf(address(aaveV3Pool));
-        aaveV3Pool.deposit(amount, user);
-        uint256 aTokenAfterDeposit = aToken.scaledBalanceOf(address(aaveV3Pool));
+    //     uint256 aTokenBefore = aToken.scaledBalanceOf(address(aaveV3Pool));
+    //     aaveV3Pool.deposit(amount, user);
+    //     uint256 aTokenAfterDeposit = aToken.scaledBalanceOf(address(aaveV3Pool));
 
-        vm.stopPrank();
-        vm.startPrank(address(aToken));
-        rewardsController.handleAction(address(aaveV3Pool), aToken.totalSupply(), aToken.scaledBalanceOf(address(aaveV3Pool)));
+    //     vm.stopPrank();
+    //     vm.startPrank(address(aToken));
+    //     rewardsController.handleAction(
+    //         address(aaveV3Pool),
+    //         aToken.totalSupply(),
+    //         aToken.scaledBalanceOf(address(aaveV3Pool))
+    //     );
 
-        vm.warp(block.timestamp + 1_000_000);
+    //     vm.warp(block.timestamp + 1_000_000);
 
-        rewardsController.handleAction(address(aaveV3Pool), aToken.totalSupply(), aToken.scaledBalanceOf(address(aaveV3Pool)));
-        vm.stopPrank();
-        startHoax(bot);
+    //     rewardsController.handleAction(
+    //         address(aaveV3Pool),
+    //         aToken.totalSupply(),
+    //         aToken.scaledBalanceOf(address(aaveV3Pool))
+    //     );
+    //     vm.stopPrank();
+    //     startHoax(bot);
 
-        address[] memory _path = new address[](2);
-        _path[0] = WETH;
-        _path[1] = address(baseToken);
-        uint256 _deadline = block.timestamp + 1000;
+    //     address[] memory _path = new address[](2);
+    //     _path[0] = WETH;
+    //     _path[1] = address(baseToken);
+    //     uint256 _deadline = block.timestamp + 1000;
 
-        aaveV3Pool.reinvest(100, _path, _deadline);
-        uint256 aTokenAfterReinvest = aToken.scaledBalanceOf(address(aaveV3Pool));
+    //     aaveV3Pool.reinvest(100, _path, _deadline);
+    //     uint256 aTokenAfterReinvest = aToken.scaledBalanceOf(address(aaveV3Pool));
 
-        console.log("LP balance in aave before deposit:", aTokenBefore);
-        console.log("LP balance in aave after deposit:", aTokenAfterDeposit);
-        console.log("LP balance in aave after reinvest:", aTokenAfterReinvest);
-    }
+    //     console.log("LP balance in aave before deposit:", aTokenBefore);
+    //     console.log("LP balance in aave after deposit:", aTokenAfterDeposit);
+    //     console.log("LP balance in aave after reinvest:", aTokenAfterReinvest);
+    // }
 
     function testWithdrawReserve() public {
         uint256 amount = 1e9;
@@ -267,7 +274,7 @@ contract AaveV3Test is Test {
 
         aaveV3Pool.deposit(amount, user);
         uint256 reserveB = aaveV3Pool.reserves(address(aToken));
-        uint256 amountToWithdraw = reserveB * 9 / 10;
+        uint256 amountToWithdraw = (reserveB * 9) / 10;
         uint256 aTokenShareBefore = aToken.scaledBalanceOf(address(aaveV3Pool));
         aaveV3Pool.withdrawReserve(address(aToken), amountToWithdraw); // withdraw 90%
         uint256 aTokenShareAfter = aToken.scaledBalanceOf(address(aaveV3Pool));
