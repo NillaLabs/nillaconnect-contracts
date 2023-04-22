50be891b798e44605a67d1c175a03524c706fe19
Sitthiput <sitthiput.m@gmail.com>
2023-04-22 17:42:30 +0700

    update: add view function to convert amt to share

--- interfaces/IstETH.sol
+++ interfaces/IstETH.sol
@@ -6,12 +6,14 @@ import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.s
 
 interface IstETH is IERC20 {
     /**
-    *  @notice Adds eth to the pool
-    *  @return StETH Amount of StETH generated
-    */
+     *  @notice Adds eth to the pool
+     *  @return StETH Amount of StETH generated
+     */
     function submit(address referral) external payable returns (uint256 StETH);
 
     function getPooledEthByShares(uint256 shares) external view returns (uint256);
+
+    function getSharesByPooledEth(uint256 _ethAmount) external view returns (uint256);
 
     function sharesOf(address account) external view returns (uint256);
 
