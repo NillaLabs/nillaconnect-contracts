70542e83d74b5ca5bcd518ae7b997d1e214d2cdf
Sitthiput <sitthiput.m@gmail.com>
2023-04-27 18:40:12 +0700

    script: DONE

--- scripts/network_optimism/upgrade_yearn.py
+++ scripts/network_optimism/upgrade_yearn.py
@@ -35,44 +35,44 @@ PERFORMANCE_FEE_BPS = 500  # 5%
 
 MULTISIG_WALLET = "0x6f650AE486eFc27BeEFb8Dc84000F63acA99735f"  # OP's Nilla
 
-accounts[0].transfer(deployer, "10 ethers")
-
 
 def main():
-    impl = YearnNillaVault.deploy({"from": deployer})
+    impl = YearnNillaVault.at("0x615D26Fc22AA78c2bc6cE324eDBD325700197301")
     admin = ProxyAdminImpl.at("0x85774d5Fc82EDC9633624c009F0edfAD2DDebA1C")
-    for nyv_token in [
-        "0x676CF057e8e275df4FCFc5d72e6a334336C28069",
-        "0x6d399C5BFd8a269a218CB3c4979D92dd9b801AC7",
-        "0x64a487DA2eDB8cB63e9b0E032DB4184C76efeec5",
-        "0xA7da71d3F65ecf86697b32dD8890d53EF49Be6F2",
-        "0x6273359be3030E1b2E14E3937af1017D6974350c",
-    ]:
-        # nilla = YearnNillaVault.at(nyv_token)
-        # partner = nilla.PARTNER_ADDRESS()
-        # yv_token = nilla.yvToken()
-        # tracker = nilla.yearnPartnerTracker()
-        # base_token = nilla.baseToken()
-        # decimals = nilla.decimals()
-        # reserves = nilla.reserves(yearn_address["USDC"])
-        # deposit_fee = nilla.depositFeeBPS()
-        # withdraw_fee = nilla.withdrawFeeBPS()
-        # worker = nilla.worker()
 
-        admin.upgrade(nyv_token, impl, {"from": deployer})
+    # admin.upgrade(nyv_token, impl, {"from": deployer})
+    # for nyv_token in [
+    #     "0x6d399C5BFd8a269a218CB3c4979D92dd9b801AC7",
+    #     "0x64a487DA2eDB8cB63e9b0E032DB4184C76efeec5",
+    #     "0xA7da71d3F65ecf86697b32dD8890d53EF49Be6F2",
+    #     "0x6273359be3030E1b2E14E3937af1017D6974350c",
+    # ]:
+    #     print("nyv_token:", nyv_token)
+    # nilla = YearnNillaVault.at(nyv_token)
+    # partner = nilla.PARTNER_ADDRESS()
+    # yv_token = nilla.yvToken()
+    # tracker = nilla.yearnPartnerTracker()
+    # base_token = nilla.baseToken()
+    # decimals = nilla.decimals()
+    # reserves = nilla.reserves(yearn_address["USDC"])
+    # deposit_fee = nilla.depositFeeBPS()
+    # withdraw_fee = nilla.withdrawFeeBPS()
+    # worker = nilla.worker()
 
-        # assert partner == nilla.PARTNER_ADDRESS()
-        # assert yv_token == nilla.yvToken()
-        # assert tracker == nilla.yearnPartnerTracker()
-        # assert base_token == nilla.baseToken()
-        # assert decimals == nilla.decimals()
-        # assert reserves == nilla.reserves(yearn_address["USDC"])
-        # assert deposit_fee == nilla.depositFeeBPS()
-        # assert withdraw_fee == nilla.withdrawFeeBPS()
-        # assert worker == nilla.worker()
-        # assert nilla.performanceFeeBPS() == 0
-        # assert nilla.principals(deployer) == 0
+    # admin.upgrade(nyv_token, impl, {"from": deployer})
 
-        YearnNillaVault.at(nyv_token).setPerformanceFeeBPS(
-            PERFORMANCE_FEE_BPS, {"from": deployer}
-        )
+    # assert partner == nilla.PARTNER_ADDRESS()
+    # assert yv_token == nilla.yvToken()
+    # assert tracker == nilla.yearnPartnerTracker()
+    # assert base_token == nilla.baseToken()
+    # assert decimals == nilla.decimals()
+    # assert reserves == nilla.reserves(yearn_address["USDC"])
+    # assert deposit_fee == nilla.depositFeeBPS()
+    # assert withdraw_fee == nilla.withdrawFeeBPS()
+    # assert worker == nilla.worker()
+    # assert nilla.performanceFeeBPS() == 0
+    # assert nilla.principals(deployer) == 0
+
+    # YearnNillaVault.at(nyv_token).setPerformanceFeeBPS(
+    #     PERFORMANCE_FEE_BPS, {"from": deployer}
+    # )
